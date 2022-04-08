bc98d6388406aafa9f9db1fd2bcfe2e87c6b8cbc
Ch Fa <79060477+chfa435@users.noreply.github.com>
2022-04-08 17:34:25 -0400

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,27 @@
+ 
+ //Step ONE (GET = controller, accepts a request)
+function getMessage(){
+
+    let msg = document.getElementById("txtMessage").value;
+
+    displayMessage(msg);
+
+}
+
+//Final Step (DISPLAY = view)
+function displayMessage(message) {
+     
+    //first get the ol element from the page
+    element = document.getElementById("results");
+    
+    //next create a new li element
+    let item = document.createElement("li");
+    //add classes to the li element
+    item.classList.add("list-group-item");
+    //set the message for the li element
+    item.innerHTML = message;
+
+    //add the new item to the list
+    element.appendChild(item);
+}
+
